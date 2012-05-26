e170bfdaa4c4f6b6d5262b583c2b8aa92af7602f
Mark T. Holder <mtholder@gmail.com>
2012-05-25 23:53:28 -0500

    improvements to exporting of env vars

--- download-dev-resource.py
+++ download-dev-resource.py
@@ -9,6 +9,14 @@ environmental variables:
         default location for config file that stores local filepaths
 
 '''
+from os.path import basename
+from urlparse import urlsplit
+import urllib2
+import urllib
+import shutil
+import subprocess
+import zipfile
+from open_tree_env import get_otol_build_env
 
 _program_name = 'download-dev-resource'
 _program_subtitle = 'OpenTree developer download utility'
@@ -21,17 +29,121 @@ _program_copyright = 'Copyright (C) 2012 Mark T. Holder.\n' \
                  'This is free software: you are free to change\nand redistribute it. ' \
                  'There is NO WARRANTY,\nto the extent permitted by law.'
 
-from open-tree-env import get_otol_build_env
 
 
 # Global dictionary of resources grouped by 'category' Note that dict is filled
 #   as a side effect of creating an OpenTreeResource
 ALL_RESOURCES = {}
 RESOURCE_CATEGORIES = {'taxonomy' : 'OPEN_TREE_TAXONOMY_DIR',
-                       'dependency' : 
-for key in RESOURCE_CATEGORIES:
+                       'dependency' : 'OPEN_TREE_DEPENDENCY_DIR'
+                       }
+for key in RESOURCE_CATEGORIES.keys():
     ALL_RESOURCES[key] = []
 SUPPORTED_PROTOCOLS = ['http', 'svn']
+
+# global config file parser
+_CFG_PARSER = None
+
+
+################################################################################
+# The following is from http://stackoverflow.com/questions/862173/how-to-download-a-file-using-python-in-a-smarter-way
+############################
+
+def url2name(url):
+    return basename(urllib.unquote(urlsplit(url)[2]))
+
+def download_http(url, localFileName=None):
+    localName = url2name(url)
+    req = urllib2.Request(url)
+    r = urllib2.urlopen(req)
+    if r.info().has_key('Content-Disposition'):
+        # If the response has Content-Disposition, we take file name from it
+        localName = r.info()['Content-Disposition'].split('filename=')[1]
+        dq = (localName[0] == '"' and localName[-1] == '"')
+        sq = (localName[0] == "'" and localName[-1] == "'")
+        if sq or dq:
+            localName = localName[1:-1]
+    elif r.url != url: 
+        # if we were redirected, the real file name we take from the final URL
+        localName = url2name(r.url)
+    if localFileName: 
+        # we can force to save the file as specified name
+        localName = localFileName
+    f = open(localName, 'wb')
+    _LOG.info('Downloading "%s" to "%s"...\n' % (localName, os.path.abspath(os.curdir)))
+    shutil.copyfileobj(r, f)
+    f.close()
+    r.close()
+    return localName
+
+############################
+# End code snippet from stackoverflow
+################################################################################
+## The following is modified from: http://code.activestate.com/recipes/252508/ (r2)
+# unzip.py
+#     Version: 1.1
+# 
+#     Extract a zipfile to the directory provided
+#     It first creates the directory structure to house the files
+#     then it extracts the files to it.
+#     
+# 
+#     By Doug Tolton
+
+
+class unzip:
+        
+    def extract(self, file, dir):
+        if not dir.endswith(':') and not os.path.exists(dir):
+            os.mkdir(dir)
+
+        zf = zipfile.ZipFile(file)
+
+        # create directory structure to house files
+        self._createstructure(file, dir)
+
+        # extract files to directory structure
+        for i, name in enumerate(zf.namelist()):
+            if not name.endswith('/'):
+                outfile = open(os.path.join(dir, name), 'wb')
+                outfile.write(zf.read(name))
+                outfile.flush()
+                outfile.close()
+
+
+    def _createstructure(self, file, dir):
+        self._makedirs(self._listdirs(file), dir)
+
+
+    def _makedirs(self, directories, basedir):
+        """ Create any directories that don't currently exist """
+        for dir in directories:
+            curdir = os.path.join(basedir, dir)
+            if not os.path.exists(curdir):
+                os.mkdir(curdir)
+
+    def _listdirs(self, file):
+        """ Grabs all the directories in the zip structure
+        This is necessary to create the structure before trying
+        to extract the file to it. """
+        zf = zipfile.ZipFile(file)
+
+        dirs = []
+
+        for name in zf.namelist():
+            if name.endswith('/'):
+                dirs.append(name)
+
+        dirs.sort()
+        return dirs
+def unzip_file(file, dest_parent):
+    u = unzip()
+    u.extract(file, dest_parent)
+## end of http://code.activestate.com/recipes/252508/ }}}
+
+class RESOURCE_STATUS_CODE:
+    SKIPPED, DOWNLOADED, INSTALLED, MISSING = range(4)
+
 
 
 class OpenTreeResource(object):
@@ -63,7 +175,7 @@ class OpenTreeResource(object):
         if self.protocol not in SUPPORTED_PROTOCOLS:
             raise ValueError('Protocol ' + protocol + ' unrecognized')
         self.category = category.lower()
-        if self.category not in RESOURCE_CATEGORIES:
+        if self.category not in RESOURCE_CATEGORIES.keys():
             raise ValueError('Category ' + category + ' unrecognized')
         self.compression = compression.lower()
         if self.compression not in OpenTreeResource.UNPACKING_PROTOCOLS:
@@ -77,11 +189,63 @@ class OpenTreeResource(object):
             self.compressed_name = self.compressed_name
         self.contact = contact
         self.description = description
+        self.path = None
+        self.installed_path = None
+        self.status = None
         # here is the hack in which we add resource to the global list
         ALL_RESOURCES[self.category].append(self)
+
     def listing(self, opts):
         return '%s = %s' % (self.name, self.description)
 
+    def do_download(self):
+        parent_var = RESOURCE_CATEGORIES[self.category]
+        parent_dir = get_otol_build_env(parent_var)
+        assert parent_dir
+        if not os.path.exists(parent_dir):
+            os.makedirs(parent_dir)
+        cwd = os.path.abspath(os.getcwd())
+        try:
+            parent_dir = os.path.abspath(parent_dir)
+            expected_path = os.path.join(parent_dir, self.compressed_name)
+            os.chdir(parent_dir)
+            if self.protocol == 'http':
+                if os.path.exists(expected_path):
+                    _LOG.warn('Path "%s" already exists. It will not be downloaded again...\n' % expected_path)
+                    fp = expected_path
+                else:
+                    p = download_http(self.url)
+                    fp = os.path.join(parent_dir, p)
+                self.compressed_path = fp
+
+                expected_path = os.path.join(parent_dir, self.name)
+                if os.path.exists(expected_path):
+                    if expected_path != fp:
+                        _LOG.warn('Path "%s" already exists. It will not be unpacked again...\n' % expected_path)
+                    fp = expected_path
+                else:
+                    if self.compression == 'zip':
+                        _LOG.info('Unzipping "%s"...\n"' % fp)
+                        unzip_file(fp, parent_dir)
+                        fp = os.path.join(parent_dir, self.name)
+                    _LOG.info('Obtained "%s"...\n"' % fp)
+                self.path = fp
+                return fp
+            elif self.protocol == 'svn':
+                if os.path.exists(expected_path):
+                    _LOG.warn('Path "%s" already exists. It will not be downloaded again...\n' % fp)
+                else:
+                    rc = subprocess.call(['svn', 'checkout', self.url, self.name])
+                    if rc != 0:
+                        raise RuntimeError('svn checkout of "%s" failed\n' % self.url)
+                self.path = expected_path
+                self.compressed_path = self.path
+            else:
+                assert False
+            self.status = RESOURCE_STATUS_CODE.DOWNLOADED
+
+        finally:
+            os.chdir(cwd)
 
 
 
@@ -96,6 +260,7 @@ OpenTreeResource(name='taxa_allCoL.tre',
                  compression='zip',
                  contact='blackrim',
                  description='Newick string representation of Catalogue of Life')
+
 OpenTreeResource(name='ncl', 
                  url='https://ncl.svn.sourceforge.net/svnroot/ncl/branches/v2.1',
                  protocol='svn',
@@ -116,17 +281,93 @@ OpenTreeResource(name='ncl',
 
 ################################################################################
 
-def list_command(opts):
-    for key in RESOURCE_CATEGORIES:
-        row = ALL_RESOURCES[key]
-        out = sys.stdout
+def get_resource_obj(name=''):
+    nl = name.lower()
+    for row in ALL_RESOURCES.itervalues():
         for resource in row:
-            out.write(resource.listing(opts))
-            out.write('\n')
-            
+            if resource.name.lower() == nl:
+                return resource
+    return None
+
+def get_flattened_resource_list():
+    r = []
+    for key in RESOURCE_CATEGORIES.keys():
+        r.extend(ALL_RESOURCES[key])
+    return r
+
+def list_command(opts):
+    '''Lists all the known resources to stdout.'''
+    out = sys.stdout
+    for resource in get_flattened_resource_list():
+        out.write(resource.listing(opts))
+        out.write('\n')
+
+
+def get_cfg_interface(cfg_path):
+    global _CFG_PARSER
+    if _CFG_PARSER is None:
+        from ConfigParser import RawConfigParser
+        c = RawConfigParser()
+        for resource in get_flattened_resource_list():
+            c.add_section(resource.name.lower())
+        if cfg_path and os.path.exists(cfg_path):
+            co = open(cfg_path)
+            c.readfp(co)
+            co.close()
+        _CFG_PARSER = c
+    return _CFG_PARSER
+        
+
+def get_resource_status_code(res_id, cfg_path, opts):
+    resource = get_resource_obj(name=res_id)
+    if resource is None:
+        raise ValueError('Resource "%s" not recognized' % res_id)
+    resource_path = None
+    cfg_interface = get_cfg_interface(cfg_path)
+    try:
+        p = cfg_interface.get(res_id.lower(), 'path')
+    except:
+        p = None
+    resource.status = RESOURCE_STATUS_CODE.MISSING
+    if p and os.path.exists(p):
+        resource.path = p
+        resource.status = RESOURCE_STATUS_CODE.DOWNLOADED
+    try:
+        p = cfg_interface.get(res_id.lower(), 'installpath')
+    except:
+        p = None
+    if p and os.path.exists(p):
+        resource.installed_path = p
+        resource.status = RESOURCE_STATUS_CODE.INSTALLED
+    return resource.status
+        
+    
+    
+    
+    
+def get_command(res_id, cfg_path, opts):
+    '''Downloads and installs the resource with name `res_id` if it is not 
+    already installed.
+    '''
+    s = get_resource_status_code(res_id, cfg_path, opts)
+    resource = get_resource_obj(name=res_id)
+    if s == RESOURCE_STATUS_CODE.INSTALLED:
+        _LOG.info('Resource "%s" already installed at "%s"' % (res_id, resource.installed_path))
+        return
+    if s == RESOURCE_STATUS_CODE.DOWNLOADED:
+        _LOG.info('Resource "%s" already obtained at "%s"' % (res_id, resource.path))
+        return
+    p = resource.do_download()
+    cfg_interface = get_cfg_interface(cfg_path)
+    cfg_interface.set(res_id.lower(), 'path', resource.path)
+    cfg_path_par = os.path.dirname(cfg_path)
+    if not os.path.exists(cfg_path_par):
+        os.makedirs(cfg_path_par)
+    with open(cfg_path, 'wb') as cfg_fileobj:
+        cfg_interface.write(cfg_fileobj)
+    
 if __name__ == '__main__':
     from optparse import OptionParser
-    from ConfigParser import ConfigParser
     import os
     import sys
     import logging
@@ -228,20 +469,22 @@ if __name__ == '__main__':
     _LOG.debug('Config path is %s\n' % cfg_path)
 
     command = args[0].lower()
-    if command == 'list':
-        list_command(opts)
-    else:
-        if len(args) < 2:
-            sys.exit('Expecting a resource identifier after the "%s" command.' % command)
-        for res_id in args[1:]:
-            if command == 'status':
-                status_command(res_id, cfg_path, opts)
-            elif command == 'get':
-                get_command(res_id, cfg_path, opts)
-            else:
-                sys.exit('command "%s" not recognized. Use -h for help' % command)
-    if not os.path.exists(cfg_path):
-        _LOG.info('Path "%s" does not exist, creating it...' % cfg_path)
+    try:
+        if command == 'list':
+            list_command(opts)
+        else:
+            if len(args) < 2:
+                sys.exit('Expecting a resource identifier after the "%s" command.' % command)
+            for res_id in args[1:]:
+                if command == 'status':
+                    status_command(res_id, cfg_path, opts)
+                elif command == 'get' or command == 'install':
+                    get_command(res_id, cfg_path, opts)
+                    
+                else:
+                    sys.exit('command "%s" not recognized. Use -h for help' % command)
+    except Exception, e:
+        sys.exit("Failing with an exception:\n    %s\n" % str(e))
             
 
 ################################################################################
--- open_tree_env.py
+++ open_tree_env.py
@@ -7,10 +7,14 @@ _DEF_OPEN_TREE_VERSION = '0.0.1'
 build_env_var_list = ['OPEN_TREE_USER_SETTINGS_DIR', 
                       'OPEN_TREE_INSTALL_DIR',
                       'OPEN_TREE_VERSION',
+                      'OPEN_TREE_BIN_DIR',
+                      'OPEN_TREE_LIB_DIR',
                       'OPEN_TREE_PKG_SHARE',
                       'OPEN_TREE_TAXONOMY_DIR',
                       'OPEN_TREE_DEPENDENCY_DIR',
-                      'OPEN_TREE_DOWNLOAD_DEV_RESOURCE_CFG']
+                      'OPEN_TREE_DOWNLOAD_DEV_RESOURCE_CFG',
+                      'OPEN_TREE_BUILD_TAG',
+                      ]
 
 def get_otol_build_env(var):
     '''Checks the environment for `var` and substitutes an appropriate default
@@ -21,6 +25,14 @@ def get_otol_build_env(var):
         return val
     if var == 'OPEN_TREE_USER_SETTINGS_DIR':
         return os.path.expanduser(os.path.join('~', '.open_tree'))
+    if var == 'OPEN_TREE_INSTALL_DIR':
+        return os.path.expanduser(os.path.join('~', 'open_tree_install'))
+    if var == 'OPEN_TREE_BIN_DIR':
+        install_dir = get_otol_build_env('OPEN_TREE_INSTALL_DIR')
+        return os.path.join(install_dir, 'bin')
+    if var == 'OPEN_TREE_LIB_DIR':
+        install_dir = get_otol_build_env('OPEN_TREE_INSTALL_DIR')
+        return os.path.join(install_dir, 'lib')
     if var == 'OPEN_TREE_INSTALL_DIR':
         return os.path.expanduser(os.path.join('~', 'open_tree_install'))
     if var == 'OPEN_TREE_VERSION':
@@ -38,17 +50,53 @@ def get_otol_build_env(var):
     if var == 'OPEN_TREE_DOWNLOAD_DEV_RESOURCE_CFG':
         p = get_otol_build_env('OPEN_TREE_USER_SETTINGS_DIR')
         return os.path.join(p, 'download-dev-resource.cfg')
+    if var == 'OPEN_TREE_BUILD_TAG':
+        return 'release'
 
     return None    
 
 if __name__ == '__main__':
     import sys
     v_list = sys.argv[1:]
+    no_args = False
     if not v_list:
+        no_args = True
         v_list = build_env_var_list
+    var_list = []
     for a in build_env_var_list:
         v = get_otol_build_env(a)
+        var_list.append([a, v, []])
+    # attempt to find the tersest abbreviations
+    if no_args:
+        for n, el in enumerate(var_list):
+            value = el[1]
+            subst = el[2]
+            longest_prefix = ''
+            longest_prefix_key = ''
+            for s_el in var_list[:n]:
+                s_v = s_el[1]
+                if value.startswith(s_v) and len(s_v) > len(longest_prefix):
+                    longest_prefix = s_v
+                    longest_prefix_key = s_el[0]
+            if longest_prefix:
+                subst = el[2]
+                del subst[:]
+                subst.append(longest_prefix_key)
+                subst.append(value[len(longest_prefix):])
+    for var_entry in var_list:
+        a, v = var_entry[:2]
         if v is None:
             sys.stdout.write('unset %s\n' % a )
         else:
+            subst = var_entry[2]
+            if subst:
+                v = '${%s}%s' % (subst[0], subst[1])
             sys.stdout.write('export %s="%s"\n' % (a, v))
+    if no_args:
+        sys.stdout.write('export PATH="${OPEN_TREE_BIN_DIR}:${PATH}"\n')
+        import platform
+        if platform.system().lower() == 'darwin':
+            lib_var = 'DYLD_LIBRARY_PATH'
+        else:
+            lib_var = 'LD_LIBRARY_PATH'
+        sys.stdout.write('export %s="${OPEN_TREE_LIB_DIR}:${%s}"\n' % (lib_var, lib_var))
